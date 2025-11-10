f708d500906766055212fad1a92f6ee4599708dc
donghyunlee-ai <donghyunlee.ai@gmail.com>
2025-11-10 10:24:44 +0000

    Deploying to gh-pages from @ donghyunlee-ai/donghyunlee-ai.github.io@b8abb2fcf5c1170b13165c9800ae1b93131e6255 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,7 +59,7 @@ ninja.data = [{
                 window.location.href = "https://www.aikoreainc.com/";
               },
             },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
