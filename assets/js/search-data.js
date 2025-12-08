c374b23a6054fe30f227640e20558b427a69fd1b
donghyunlee-ai <donghyunlee.ai@gmail.com>
2025-12-08 05:01:17 +0000

    Deploying to gh-pages from @ donghyunlee-ai/donghyunlee-ai.github.io@46472a30d6c50d7997339185b2c67d77fa8fe88d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects-amp-capabilities",
-          title: "Projects &amp; Capabilities",
-          description: "R&amp;D capabilities and strategic partnerships of the Donghyun Lee AI Group.",
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "Overview of research projects, R&amp;D capabilities, and collaborative networks of the Donghyun Lee AI Group.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -496,11 +496,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-11-27-home-comming-day/";
-            },},{id: "news-news-welcome-new-undergraduate-researchers",
-          title: '👏 [News] Welcome New Undergraduate Researchers!',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/2025-12-08-welcome-new-members/";
             },},{id: "projects-ai-for-algal-bloom-forecasting",
           title: 'AI for Algal Bloom Forecasting',
           description: "Developing a Trustworthy AI model to predict harmful algal blooms (HABs).",
